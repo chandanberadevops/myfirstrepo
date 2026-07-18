39cc32a2b78f188965a593159265c2f7f315d345
chandanberadevops <chandanbera@rocketmail.com>
2026-07-18 20:21:06 +0530

    upload basic provider code

---
+++ 1_Basic_Provider/Main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.70.0"
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+provider "azurerm" {
+  features {}
+}
+
+
